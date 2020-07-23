0fb9d50f0bfd0d0114cf400f065d9688a1e0b34b
Dsanatar <danny.sanatar@gmail.com>
2020-07-23 16:08:02 -0400

    command for submitting answers and scoring skeleton

--- index.js
+++ index.js
@@ -10,20 +10,56 @@ app.get('/', function(req, res){
 var rooms = ['Team1', 'Team2'];
 var usernames = {};
 
+var ansList = {};
+
 io.sockets.on('connection', function(socket){
 
     socket.on('addUser', function(username){
         socket.username = username;
-        socket.room = 't1';
+        socket.room = 'Team1';
         usernames[username] = username;
-        socket.join('t1');
+        socket.join('Team1');
         socket.emit('updateChat', username, ' has joined Team1');
+        socket.broadcast.to('Team1').emit('updateChat', username, ' has joined Team1');
         socket.emit('updateRooms', rooms, 'Team1');
     });
 
     socket.on('sendChat', function(data){
         io.sockets.in(socket.room).emit('updateChat', socket.username, data);
     });
+
+    socket.on('sendScoreTo2', function(data){
+        socket.to('Team1').emit('updateScore', data);
+        io.sockets.in(socket.room).emit('updateScore', data);
+    });
+
+    socket.on('sendScoreTo1', function(data){
+        socket.to('Team1').emit('updateScore', data);
+        io.sockets.in(socket.room).emit('updateScore', data);
+    });
+
+    socket.on('submitAns', function(data){
+        ansList[socket.room] = data;
+        console.log(ansList)
+        if(Object.keys(ansList).length == 2){
+            switch (socket.room) {
+                case 'Team1':
+                    socket.to('Team2').emit('updateAns', ansList);
+                    io.sockets.in(socket.room).emit('updateAns', ansList);
+                    ansList = {};
+                    break;
+                case 'Team2':
+                    socket.to('Team1').emit('updateAns', ansList);
+                    io.sockets.in(socket.room).emit('updateAns', ansList);
+                    ansList = {};
+                    break;
+            
+                default:
+                    break;
+            }
+        }
+    })
+
 
     socket.on('switchRoom', function(newRoom){
         socket.leave(socket.room);
@@ -38,7 +74,7 @@ io.sockets.on('connection', function(socket){
     socket.on('disconnect', function(username){
         delete usernames[socket.username];
         io.sockets.emit('updateUsers', usernames);
-        io.emit('is-online', '<i>' + socket.username + ' left the chat </i>');
+        socket.broadcast.emit('updateChat', socket.username, ' has disconnected');
         socket.leave(socket.room);
     });
 
